96391de5e387fd7aef2300500a4f2acf08e367d3
Timur <t.akbari@web.de>
2024-04-01 17:27:01 +0200

    feat: add delete todos. persist todos in txt file

--- todo_manager/main.py
+++ todo_manager/main.py
@@ -7,19 +7,43 @@ footer = open('templates/page_footer.html', 'r').read()
 
 form = open('templates/todo_form.html', 'r').read()
 
-doings = []
+
+def get_todo_edit_form(idx: int) -> str:
+    return f'''
+    <form action="/" method="post">
+        <input type="hidden" name="delete_doing" value="{idx}">
+        <input type="submit" value="X">
+    </form>
+    '''
+
+
+def get_todos(doings: list) -> str:
+    todos = '<ul>'
+
+    for idx, doing in enumerate(doings):
+        todos += ('<li>' + doing + get_todo_edit_form(idx) + '</li>')
+
+    todos += '</ul>' + '<br>'
+
+    return todos
 
 
 @app.route('/', methods=['GET', 'POST'])
-def todo():
+def main() -> str:
+    todos_file = open('todos.txt', 'r')
+    doings = todos_file.readlines()
+    todos_file.close()
+
     if request.method == 'POST':
-        doings.append(request.form['doing'])
+        if 'add_doing' in request.form and request.form['add_doing'] != '':
+            doings.append(request.form['add_doing'])
+        if 'delete_doing' in request.form:
+            del doings[int(request.form['delete_doing'])]
 
-    todos = '<ul>'
+    todos = get_todos(doings)
 
-    for doing in doings:
-        todos += '<li>' + doing + '</li>'
-
-    todos += '</ul>' + '<br>'
+    todos_file = open('todos.txt', 'w')
+    todos_file.writelines(doings)
+    todos_file.close()
 
     return header + todos + form + footer
